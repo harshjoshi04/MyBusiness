38d1880f38c2e2282c3b11bf9eb7b17d7224ee0b
Harsh Joshi <hj201812@gmail.com>
2024-05-31 15:08:55 +0530

    Update

--- src/lib/apiRoute.ts
+++ src/lib/apiRoute.ts
@@ -1,9 +1,9 @@
 const MAIN = process.env.NEXT_URL!;
 const API = {
-  USER: `${MAIN}/api/user`,
-  CATEGORY: `${MAIN}/api/category`,
-  PRODUCT: `${MAIN}/api/product`,
-  STOCK: `${MAIN}/api/product/stock`,
+  USER: `/api/user`,
+  CATEGORY: `/api/category`,
+  PRODUCT: `/api/product`,
+  STOCK: `/api/product/stock`,
 };
 
 export default API;
