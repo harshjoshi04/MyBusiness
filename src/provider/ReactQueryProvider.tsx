c24242b12cf9f5a4322b2a1d7c8aacbaa08ea792
Harsh Joshi <hj201812@gmail.com>
2024-05-31 10:12:18 +0530

    Upload

--- src/provider/ReactQueryProvider.tsx
+++ src/provider/ReactQueryProvider.tsx
@@ -5,8 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 
-const client = new QueryClient();
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const [client] = React.useState(() => new QueryClient());
   return (
     <QueryClientProvider client={client}>
       <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
